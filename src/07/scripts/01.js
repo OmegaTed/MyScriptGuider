bd9c6048bcec73d9b503f1a41daccf9a7ca3df0e
NickTown <cocotz@163.com>
2017-07-10 17:08:27 +0800

    add the seventh chapter

---
+++ src/07/scripts/01.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/7/10.
+ */
+
+var empty = [];
+var primes = [2, 3, 5, 7, 11, 13, 17];
+var misc = [1.1, true, "this is joke"];
+var baseInt = 123;
+var baseArr = [baseInt, baseInt + 1, baseInt + 2, baseInt + 3];
